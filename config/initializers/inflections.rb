5b3a40151de0d3d51cf2c0b9eb66c3bbab8f8907
Shane Short <shanes@webinabox.net.au>
2024-01-20 15:31:06 +0800

    Upgrading to rails 7.1

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,5 +1,13 @@
-# These inflection rules are supported but not enabled by default:
+# Be sure to restart your server when you modify this file.
+
+# Add new inflection rules using the following format. Inflections
+# are locale specific, and you may define rules for as many different
+# locales as you wish. All of these examples are active by default:
 ActiveSupport::Inflector.inflections(:en) do |inflect|
+  # inflect.plural /^(ox)$/i, "\\1en"
+  # inflect.singular /^(ox)en/i, "\\1"
+  # inflect.irregular "person", "people"
+  # inflect.uncountable %w( fish sheep )
   inflect.acronym "ICAO"
   inflect.acronym "FIR"
   inflect.acronym 'ACARS'
@@ -7,3 +15,8 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
 
   inflect.uncountable %w(aircraft)
 end
+
+# These inflection rules are supported but not enabled by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym "RESTful"
+# end
