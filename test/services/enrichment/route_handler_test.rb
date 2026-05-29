a2172fff65c86a6b3e52f1c36e29747c2c60d144
Tim Raphael <raphael.timothy@gmail.com>
2026-05-29 13:01:04 +1000

    feat(enrichment): add aircraft, route and airport handlers

---
+++ test/services/enrichment/route_handler_test.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Enrichment::RouteHandlerTest < ActiveSupport::TestCase
+  test 'returns found route for a known callsign' do
+    reply = JSON.parse(Enrichment::RouteHandler.new.call({ callsign: 'AA1' }.to_json))
+
+    assert_equal true, reply['found']
+    assert_equal 'AA1', reply['route']['callsign']
+  end
+
+  test 'returns found:false for an unknown callsign' do
+    reply = JSON.parse(Enrichment::RouteHandler.new.call({ callsign: 'ZZ999' }.to_json))
+
+    assert_equal({ 'found' => false }, reply)
+  end
+
+  test 'returns bad_request when callsign is missing' do
+    reply = JSON.parse(Enrichment::RouteHandler.new.call({}.to_json))
+
+    assert_equal 'bad_request', reply['code']
+  end
+end
